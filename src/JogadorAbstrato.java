9c8bace9c3126a8d6e8ccd63d567177ea04c1b3b
vanderjus <116886009+vanderjus@users.noreply.github.com>
2023-02-20 15:29:42 -0300

    corpo básico

--- src/JogadorAbstrato.java
+++ src/JogadorAbstrato.java
@@ -1,3 +1,42 @@
 public class JogadorAbstrato {
+  private String nome;
+	private Baralho monte;
+	private int pontuacao;
+	    
+		
+	public JogadorAbstrato(String nome) {
+		super();
+		this.nome = nome;
+	}
+	
+	public void incrementarPontuacao() {
+	        this.pontuacao++;
+	}
+  
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Baralho getMonte() {
+		return monte;
+	}
+
+	public void setMonte(Baralho monte) {
+		this.monte = monte;
+	}
+
+
+	public int getPontuacao() {
+		return pontuacao;
+	}
+
+
+	public void setPontuacao(int pontuacao) {
+		this.pontuacao = pontuacao;
+	}
 
 }
